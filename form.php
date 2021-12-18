ef56b53cc3a57a7c7cc8f4aed758ee8b2f766c51
kamaalix <kamaalix@gmail.com>
2021-12-18 20:00:22 +0330

    install git

---
+++ form.php
@@ -0,0 +1,14 @@
+<form action='form.php' method='GET'>
+    <input type='text' name='username'>
+    <input type='submit'>
+</form>
+<?php
+// $_GET ['form.php'];
+if ( $_GET ['username'] == 'jack') {
+    echo 'hello' . $_GET ['username'];
+}
+else{
+    echo 'bye bye' . $_GET ['username'];
+}
+?>
+<?php echo 'hello GitHub'; ?>
